21bfa2b6e44974b9f1c14ec55c1957ba03105e54
JANDERSON-PC\JANDERSON <jssystems2021@gmail.com>
2020-10-15 08:54:43 -0300

    Testando a primeira classe de dominio - Categoria

---
+++ curso_spring/src/main/java/com/senaisp/curso_spring/domain/Categoria.java
@@ -0,0 +1,60 @@
+package com.senaisp.curso_spring.domain;
+
+import java.io.Serializable;
+
+public class Categoria implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	private String nomeString;
+	
+	public Categoria() {
+		
+	}
+	
+	public Categoria(Integer id, String nomeString) {
+		this.id = id;
+		this.nomeString = nomeString;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNomeString() {
+		return nomeString;
+	}
+	public void setNomeString(String nomeString) {
+		this.nomeString = nomeString;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+}
--- curso_spring/src/main/java/com/senaisp/curso_spring/resources/CategoriaResource.java
+++ curso_spring/src/main/java/com/senaisp/curso_spring/resources/CategoriaResource.java
@@ -1,15 +1,24 @@
 package com.senaisp.curso_spring.resources;
 
+import java.util.ArrayList;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.senaisp.curso_spring.domain.Categoria;
 
 @RestController// Notações utilizando @ são do framework para mudanças do comportamento dos mestodos, classes,parametros...
 @RequestMapping(value = "/categorias") // Chamada do metodo pela Browser Localhost:8081/categorias. 
 public class CategoriaResource {
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "rest Está funcionando";
+	public ArrayList<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "informatica");
+		Categoria cat2 = new Categoria(2, "escritorio");
+		ArrayList<Categoria> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		return list;
 	}
 }
  
